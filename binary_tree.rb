2524588facf26859a069e218de2228afd15390ac
NathanD <nathand50444@gmail.com>
2024-08-28 21:06:04 +0100

    insert and find method created
    pseudocode developed for other methods to build

--- binary_tree.rb
+++ binary_tree.rb
@@ -23,6 +23,67 @@ class Tree
     pretty_print_node(@root, '', true)
   end
 
+  def insert(data)
+    # Traverse the branches of the tree based on whether data is less than or greater than the current node.
+    # If the data is less than the current node, traverse the left branch
+    # If the data is greater than the current node, traverse the right branch
+    # If the current node is nil, create a new node with the data and return the new node.
+    current = @root
+    parent = nil
+    
+    while current
+      parent = current
+      current = data < current.data ? current.left : current.right
+    end
+    
+    new_node = Node.new(data)
+    if parent.nil?
+      @root = new_node
+    else
+      new_node.parent = parent
+      if data < parent.data
+        parent.left = new_node
+      else
+        parent.right = new_node
+      end
+    end
+  end
+
+  def delete(data)
+    # Traverse the branches of the tree based on whether data is less than or greater than the current node.
+    # If the data is less than the current node, traverse the left branch
+    # If the data is greater than the current node, traverse the right branch
+    # If the current node is nil, return nil
+    # If the current node has no children, delete the current node
+    # If the current node has one child, replace the current node with the child
+    # If the current node has two children, replace the current node with the in-order successor
+  end
+
+  def find(data)
+    # Traverse the branches of the tree based on whether data is less than or greater than the current node.
+    # If the data is less than the current node, traverse the left branch
+    # If the data is greater than the current node, traverse the right branch
+    # If the current node is nil, return nil
+    # If the current node is the data, return the current node 
+    # (what information does this give us? Can we return the Node ID?)
+    current = @root
+    
+    while current
+      if current.data == data
+        return current
+      else
+        current = data < current.data ? current.left : current.right
+      end
+    end
+    nil
+  end
+
+  def level_order
+    # Node elements should be compiled into an array in "level order"
+    # For examples, the first level should be the root node.
+    # The second and third levels should be the left and right children of the root node, etc.
+  end
+
   private
 
   def pretty_print_node(node, prefix, is_left)
@@ -33,8 +94,3 @@ class Tree
     pretty_print_node(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
   end
 end
-
-# Example
-test = Tree.new
-test.build_tree([1, 2, 3, 4, 5, 6, 7, 8, 9])
-test.pretty_print
